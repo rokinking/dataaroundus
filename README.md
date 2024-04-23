2e21891172aec6ff3f08e28fc080a24abac8fcfc
Jäger Dániel <jager.daniel97@gmail.com>
2024-04-23 10:02:31 +0200

    README.md

---
+++ README.md
@@ -0,0 +1,16 @@
+The Witcher
+Andrzej Sapkowski
+1992
+Geralt of Rivia
+Yennefer of Vengerberg
+Vilgefortz of Roggevin
+Triss Merigold
+
+
+3 Body Problem
+Liu Cixin
+2008
+Ye Wenjie
+Thomas Wade
+Wang Miao
+Pan Han
---
+++ data.js
@@ -0,0 +1,14 @@
+let title = "The Witcher"
+console.log(title)
+let author = "Andrzej Sapkowski"
+console.log(author)
+let year = 1992
+console.log(year)
+let isNewerThan2000 = false
+console.log(isNewerThan2000)
+let age = (2024 - 1992)
+console.log(age)
+let characters = ["Geralt of Rivia", "Yennefer of Vengerberg", "Triss Merigold", "Vilgefortz of Roggevin"]
+console.log(characters)
+console.log(characters[0])
+console.log(characters[1])
